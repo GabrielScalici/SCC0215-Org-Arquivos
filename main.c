920246c64dd8745792f4ab0b3845ebfd736ecf08
GabrielScalici <gabrielhc.scalici@gmail.com>
2018-04-11 00:17:45 -0300

    Criacao do menu

--- main.c
+++ main.c
@@ -7,13 +7,16 @@
 void menu()
 {
     printf("\n\tMENU\n");
-    printf("1. \n");
-    printf("2. \n");
-    printf("3. \n");
-    printf("4. \n");
-    printf("5. \n");
-    printf("6. \n");
-    printf("0. \n");
+    printf("1. LER REGISTROS\n");
+    printf("2. BUSCAR TODOS OS REGISTROS\n");
+    printf("3. BUSCAR REGISTROS COM CRITERIO DE BUSCA\n");
+    printf("4. VER REGISTROS PELO RRN\n");
+    printf("5. REMOVER REGISTROS PELO RRN\n");
+    printf("6. INSERIR REGISTRO\n");
+    printf("7. ATUALIZAR REGISTRO\n");
+    printf("8. COMPACTAR ARQUIVO\n");
+    printf("9. REGISTROS REMOVIDOS\n");
+    printf("0. SAIR DO PROGRAMA\n");
     printf("Opcao desejada: ");
 }
 
