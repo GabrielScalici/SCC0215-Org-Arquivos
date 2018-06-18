9342b10824cdf4a856370a8c9f3a6672ae37d3f9
GabrielScalici <gabrielhc.scalici@gmail.com>
2018-06-18 16:27:49 -0300

    novo trabalho iniciado

---
+++ newT2/registro.h
@@ -0,0 +1,54 @@
+#ifndef REGISTRO_H
+#define REGISTRO_H
+#include <stdio.h>
+
+//Definindo o tamanho do registro
+#define TAM_REG 87
+//Definindo o delimitador de campo
+#define DEL_CAMPO 59
+//Definições para verificação de arquivo
+enum{
+    CARREGANDO,
+    RECUPERANDO
+};
+
+
+/*
+ *  ESTRUTURAS
+ */
+
+ //Definindo o cabecalho
+ typedef struct{
+      char status;
+      int topoPilha;
+ } Cabecalho;
+
+//Definindo os registros
+typedef struct{
+    int codINEP;
+    char* dataAtiv;
+    char* uf;
+    int tamEscola;
+    char* nomEscola;
+    int tam_municipio;
+    char* municipio;
+    int tam_prestadora;
+    char* prestadora;
+} Registro;
+
+/*
+ *  FUNCOES
+ */
+FILE* cria_arquivo();
+void verifica_arquivo(FILE *f, int func);
+Registro* recuperar_registros(FILE *f,int qtdRegs);
+void transfere_arquivo(Registro* reg, int qtdRegs);
+void busca_rrn(int RRN);
+void remover_registro_rrn(int RRN);
+void printa_arquivo();
+void recupera_rrn();
+void inserir_registro(int cod, char data[10], char uf[2], char* nome_esc, char* muni, char* prest);
+void atualizar_registro(int rrn, int cod, char data[10], char uf[2], char* nome_esc, char* muni, char* prest);
+void busca_rrn_parametro(char* campo, char* valor);
+
+#endif
